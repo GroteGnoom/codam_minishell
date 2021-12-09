f3cf5dc56da6341c9ecb88ee8b258920bde927d7
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2021-12-09 12:19:46 +0100

    Fixed export.c

--- export.c
+++ export.c
@@ -1,7 +1,10 @@
 #include "minishell.h"
 #include "Libft/libft.h"
 #include <termios.h>
+#include <stdio.h>
 static int	export_attribute(t_env *s_env, char *attr);
+
+static void	ft_free(t_env *s_env);
 
 int	ft_export(char **args, t_env *s_env)
 {
@@ -21,7 +24,7 @@ int	ft_export(char **args, t_env *s_env)
 		env_str = ft_strnstr(s_env->env[i], env, ft_strlen(s_env->env[i]));
 		i++;
 	}
-	s_env->env[i] = args[1];
+	s_env->env[i - 1] = args[1];
 	return (0);
 }
 
@@ -32,20 +35,52 @@ void	copy_env(char **envp, t_env *s_env)
 	i = 0;
 	while (envp[i])
 		i++;
-	s_env->env = malloc((i + 1) * sizeof(char *));
+	s_env->env = ft_calloc((i + 1) * sizeof(char *), 1);
+	s_env->size = i;
 	i = 0;
 	while (envp[i])
 	{
 		s_env->env[i] = ft_strdup(envp[i]);
 		i++;
 	}
-	s_env->env[i] = NULL;
-	s_env->size = i;
 }
 
 static int	export_attribute(t_env *s_env, char *attr)
 {
-	(void)s_env;
-	(void)attr;
+	int		i;
+	char	**copy;
+
+	i = 0;
+	while (s_env->env[i])
+		i++;
+	if (i == s_env->size)
+	{
+		copy = ft_calloc((s_env->size + 11) * sizeof(char *), 1);
+		i = 0;
+		while (s_env->env[i])
+		{
+			copy[i] = ft_calloc(ft_strlen(s_env->env[i]) + 1, 1);
+			ft_memcpy(copy[i], s_env->env[i], ft_strlen(s_env->env[i]));
+			i++;
+		}
+		ft_free(s_env);
+		s_env->env = copy;
+		s_env->size += 10;
+	}
+	s_env->env[i] = ft_calloc(ft_strlen(attr) + 1, 1);
+	ft_memmove(s_env->env[i], attr, ft_strlen(attr));
 	return (0);
+}
+
+static void	ft_free(t_env *s_env)
+{
+	int	i;
+
+	i = 0;
+	while (s_env->env[i])
+	{
+		free(s_env->env[i]);
+		i++;
+	}
+	free(s_env->env);
 }
