764bd9ce16f06c0af1cb82443693474e371b0c5f
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2021-12-09 14:10:37 +0100

    Fixed unset.c

--- unset.c
+++ unset.c
@@ -2,5 +2,18 @@
 #include "Libft/libft.h"
 int	ft_unset(char **args, t_env *s_env)
 {
+	char	*env;
+	int		i;
+
+	i = 0;
+	while (i < s_env->size && \
+	!ft_strnstr(s_env->env[i], args[1], ft_strlen(args[1])))
+		i++;
+	if (i >= s_env->size)
+		return (1);
+	env = ft_strchr(s_env->env[i], '=');
+	if (env != 0)
+		ft_substr_free(&s_env->env[i], 0, \
+		ft_strlen(s_env->env[i]) - ft_strlen(env));
 	return (0);
 }
