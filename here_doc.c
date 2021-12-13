5f21d43f135c128e03162205e79a90af671f279e
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2021-12-13 11:45:38 +0100

    Fixed here_doc

--- here_doc.c
+++ here_doc.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 09:52:34 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2021/12/13 10:11:35 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2021/12/13 11:33:24 by sde-rijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,58 @@
 #include <stdio.h>
 #include <readline/readline.h>
 
+void	ft_redir_args(char **args);
+
 char	**here_doc(char *final)
 {
 	char	**args;
 	char	*line;
 	int		size;
 
-	size = 2;
 	line = readline("here_doc> ");
+	size = 2;
 	args = malloc(1 * sizeof(char *));
 	while (line)
 	{
 		if (!ft_strcmp(line, final))
-		{
-			return (args);
-		}
+			break ;
 		args = ft_realloc(args, (size * sizeof(char *)), \
 		((size - 1) * sizeof(char *)));
 		args[size - 1] = NULL;
-		args[size - 2] = ft_strdup(line);
+		args[size - 2] = ft_strjoin(line, "\n");
 		free(line);
 		line = readline("here_doc> ");
 		size++;
 	}
+	ft_redir_args(args);
 	return (0);
+}
+
+void	ft_redir_args(char **args)
+{
+	int		pipefd[2];
+	int		term;
+	int		i;
+
+	i = 0;
+	term = 0;
+	pipe(pipefd);
+	if (dup2(STDOUT_FILENO, term) < 0)
+		return ;
+	if (dup2(pipefd[1], STDOUT_FILENO) < 0)
+		return ;
+	close(pipefd[0]);
+	i = 0;
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], STDIN_FILENO);
+		i++;
+	}
+	close(STDIN_FILENO);
+	if (dup2(term, STDOUT_FILENO) < 0)
+		return ;
+	if (dup2(pipefd[0], STDIN_FILENO) < 0)
+		return ;
+	close(pipefd[0]);
+	close(pipefd[1]);
 }
