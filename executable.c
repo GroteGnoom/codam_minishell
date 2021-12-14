8b7984daa5c13f124a807f67ba000023364d127d
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2021-12-14 10:44:37 +0100

    Add parts to executable.c

--- executable.c
+++ executable.c
@@ -6,20 +6,24 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:16:40 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2021/12/13 10:16:45 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2021/12/14 10:19:34 by sde-rijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "Libft/libft.h"
 
-int	ft_executable(char **args, t_env *s_env)
+static char	**ft_get_args(int nr_parts, t_part *parts);
+
+int	ft_executable(int nr_parts, t_part *parts, t_env *s_env)
 {
 	pid_t	child;
+	char	**args;
 	char	**paths;
 	int		status;
 
 	paths = ft_get_paths(s_env->env);
+	args = ft_get_args(nr_parts, parts);
 	child = fork();
 	if (child < 0)
 		return (1);
@@ -27,4 +31,20 @@ int	ft_executable(char **args, t_env *s_env)
 		ft_try_paths(paths, args, s_env->env);
 	waitpid(-1, &status, 0);
 	return (WEXITSTATUS(status));
+}
+
+static char	**ft_get_args(int nr_parts, t_part *parts)
+{
+	char	**args;
+	int		i;
+
+	i = 0;
+	args = ft_calloc((nr_parts + 1) * sizeof(char *), 1);
+	while (i < nr_parts)
+	{
+		args[i] = ft_strdup(parts->part);
+		parts++;
+		i++;
+	}
+	return (args);
 }
