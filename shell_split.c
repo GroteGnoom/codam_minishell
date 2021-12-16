e32cefe91b676e821e39c87ad63ce5303b0c53be
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-12-16 16:34:01 +0100

    do not combine wildcard parts

--- shell_split.c
+++ shell_split.c
@@ -6,7 +6,7 @@
 /*   By: dnoom <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/16 11:47:17 by dnoom         #+#    #+#                 */
-/*   Updated: 2021/12/16 13:35:52 by dnoom         ########   odam.nl         */
+/*   Updated: 2021/12/16 16:28:16 by dnoom         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ int	count_combined_parts(t_part *parts)
 			j++;
 		if (parts[i].type == SPECIAL)
 			j += 2;
+		if (i != 0 && parts[i].type == NORMAL && parts[i - 1].type == NORMAL)
+			j += 2;
 		i++;
 	}
 	return (j);
@@ -44,7 +46,8 @@ int	combine_parts(t_part *parts, t_part *outparts, int i, int j)
 	else if (parts[i].type != SPACES)
 	{
 		if (i == 0 || (parts[i - 1].type == SPACES || \
-					parts[i - 1].type == SPECIAL))
+				(parts[i - 1].type == NORMAL && parts[i].type == NORMAL) || \
+				parts[i - 1].type == SPECIAL))
 			outparts[++j].part = ft_calloc(1, 1);
 		outparts[j].type = NORMAL;
 		ft_strjoin_free(&(outparts[j].part), parts[i].part);
