8f1dbe83f77ee40e5dfce60d9c8e8ded05125039
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-12-16 11:58:55 +0100

    norminette of shell_split

--- shell_split.c
+++ shell_split.c
@@ -6,7 +6,7 @@
 /*   By: dnoom <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/16 11:47:17 by dnoom         #+#    #+#                 */
-/*   Updated: 2021/12/16 11:49:50 by dnoom         ########   odam.nl         */
+/*   Updated: 2021/12/16 11:55:59 by dnoom         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,25 @@ int	count_combined_parts(t_part *parts)
 	return (j);
 }
 
+int	combine_parts(t_part *parts, t_part *outparts, int i, int j)
+{
+	if (parts[i].type == SPECIAL)
+	{
+		outparts[++j].part = ft_calloc(1, 1);
+		ft_strjoin_free(&(outparts[j].part), parts[i].part);
+		outparts[j].type = SPECIAL;
+	}
+	else if (parts[i].type != SPACES)
+	{
+		if (i == 0 || (parts[i - 1].type == SPACES || \
+					parts[i - 1].type == SPECIAL))
+			outparts[++j].part = ft_calloc(1, 1);
+		outparts[j].type = NORMAL;
+		ft_strjoin_free(&(outparts[j].part), parts[i].part);
+	}
+	return (j);
+}
+
 t_part	*ft_shell_split(char *s, int last_exit_status, t_env *s_env)
 {
 	t_part	*parts;
@@ -49,20 +68,7 @@ t_part	*ft_shell_split(char *s, int last_exit_status, t_env *s_env)
 	j = -1;
 	while (parts[i].part)
 	{
-		if (parts[i].type == SPECIAL)
-		{
-			outparts[++j].part = ft_calloc(1, 1);
-			ft_strjoin_free(&(outparts[j].part), parts[i].part);
-			outparts[j].type = SPECIAL;
-		}
-		else if (parts[i].type != SPACES)
-		{
-			if (i == 0 || (parts[i - 1].type == SPACES || \
-			parts[i - 1].type == SPECIAL))
-				outparts[++j].part = ft_calloc(1, 1);
-			outparts[j].type = NORMAL;
-			ft_strjoin_free(&(outparts[j].part), parts[i].part);
-		}
+		j = combine_parts(parts, outparts, i, j);
 		i++;
 	}
 	ft_free_parts(parts);
