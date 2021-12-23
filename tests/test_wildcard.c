243ffe01b787da3c16542f7410db91f938ceb49b
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-12-23 10:12:15 +0100

    fix test_wildcard to work with quotes

--- tests/test_wildcard.c
+++ tests/test_wildcard.c
@@ -6,7 +6,7 @@
 /*   By: dnoom <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/15 14:35:14 by dnoom         #+#    #+#                 */
-/*   Updated: 2021/12/16 15:55:32 by dnoom         ########   odam.nl         */
+/*   Updated: 2021/12/23 10:08:22 by daniel        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,26 @@
 
 int	main(void)
 {
-	char	*result;
+	char		*result;
+	static int	quoted1[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	static int	quoted2[] = {1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0};
 
-	result = ft_wildcard(ft_strdup("test_wi*c"));
+	result = ft_wildcard(ft_strdup("test_wi*c"), quoted1);
 	printf("%s\n\n", result);
 	free(result);
-	result = ft_wildcard(ft_strdup("test_wi*.c"));
+	result = ft_wildcard(ft_strdup("test_wi*.c"), quoted1);
 	printf("%s\n\n", result);
 	free(result);
-	result = ft_wildcard(ft_strdup("*wildcard.c"));
+	result = ft_wildcard(ft_strdup("*wildcard.c"), quoted1);
 	printf("%s\n\n", result);
 	free(result);
-	result = ft_wildcard(ft_strdup("*wild*a*r*.c"));
+	result = ft_wildcard(ft_strdup("*wild*a*r*.c"), quoted1);
 	printf("%s\n\n", result);
 	free(result);
-	result = ft_wildcard(ft_strdup("*wild**.c"));
+	result = ft_wildcard(ft_strdup("*wild**.c"), quoted1);
+	printf("%s\n\n", result);
+	free(result);
+	result = ft_wildcard(ft_strdup("*wild**.c"), quoted2);
 	printf("%s\n\n", result);
 	free(result);
 }
