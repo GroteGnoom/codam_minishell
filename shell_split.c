6bdf3f37b2b485f457d5bc3eb021c637df66592f
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-12-23 10:48:13 +0100

    fix combine_parts

--- shell_split.c
+++ shell_split.c
@@ -6,7 +6,7 @@
 /*   By: dnoom <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/16 11:47:17 by dnoom         #+#    #+#                 */
-/*   Updated: 2021/12/20 15:45:49 by daniel        ########   odam.nl         */
+/*   Updated: 2021/12/23 10:34:55 by daniel        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,8 @@ int	count_combined_parts(t_part *parts)
 void	combine_parts(t_part *parts, t_part *outparts, int i, int *j, int **wild_quoted)
 {
 	int	length;
+	int	quote;
+	int	k;
 
 	if (parts[i].type == SPECIAL)
 	{
@@ -53,16 +55,22 @@ void	combine_parts(t_part *parts, t_part *outparts, int i, int *j, int **wild_qu
 				parts[i - 1].type == SPECIAL))
 		{
 			outparts[++(*j)].part = ft_calloc(1, 1);
-			wild_quoted[(*j)] = ft_calloc(1, 1);
+			wild_quoted[(*j)] = ft_calloc(1, sizeof(int));
 		}
 		outparts[(*j)].type = NORMAL;
 		length = ft_strlen(outparts[(*j)].part);
-		ft_realloc(wild_quoted[(*j)], length + ft_strlen(parts[i].part), length);
+		wild_quoted[(*j)] = ft_realloc(wild_quoted[(*j)], (length + ft_strlen(parts[i].part)) * sizeof(int), length * sizeof(int));
 		ft_strjoin_free(&(outparts[(*j)].part), parts[i].part);
 		if (parts[i].type == SINGLE_QUOTED || parts[i].type == DOUBLE_QUOTED)
-			ft_memset(wild_quoted[(*j)] + length, 1, ft_strlen(parts[i].part));
+			quote = 1;
 		else
-			ft_memset(wild_quoted[(*j)] + length, 1, ft_strlen(parts[i].part));
+			quote = 0;
+		k = 0;
+		while (k < (int)ft_strlen(parts[i].part))
+		{
+			*(wild_quoted[(*j)] + length + k) = quote;
+			k++;
+		}
 	}
 }
 
@@ -87,8 +95,8 @@ t_part	*ft_shell_split(char *s, int last_exit_status, t_env *s_env)
 		combine_parts(parts, outparts, i, &j, wild_quoted);
 		i++;
 	}
-	expand_wildcard(&parts, wild_quoted);
+	expand_wildcard(&outparts, wild_quoted);
 	ft_free_parts(parts);
-	//ft_free_strs((char**)wild_quoted);
+	ft_free_strs((char**)wild_quoted);
 	return (outparts);
 }
