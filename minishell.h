7717806c8f1f37bb9b03cca2e8b0bed3514e95ad
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2021-12-28 14:03:21 +0100

    add line nr to header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:16:05 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2021/12/28 11:46:40 by daniel        ########   odam.nl         */
+/*   Updated: 2021/12/28 14:02:40 by daniel        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,11 +99,11 @@ void	expand_wildcard(t_part **parts, int **wild_quoted);
 
 int		is_built_in(char *str, int nr_parts, t_part *parts, t_env *s_env);
 
-int		ft_syntax_error(t_part *parts, int i);
-int		ft_redir_error(char *str, char *str2);
-int		ft_invalid_identifier(t_part *parts, int i);
+int		ft_syntax_error(t_part *parts, int i, int line_nr);
+int		ft_redir_error(char *str, char *str2, int line_nr);
+int		ft_invalid_identifier(t_part *parts, int i, int line_nr);
 int		check_identifier(char *str, int unset);
-int		ft_exit_error(int too_many, char *arg);
+int		ft_exit_error(int too_many, char *arg, int line_nr);
 
 void	print_parts(t_part *parts);
 void	print_strs(char **strs);
