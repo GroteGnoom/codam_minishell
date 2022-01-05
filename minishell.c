5c7767ba208ea25c68f5c29f6238258dae0245ae
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-01-05 10:32:14 +0100

    remove | "|" syntax error, add test

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:16:10 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/05 10:21:30 by daniel        ########   odam.nl         */
+/*   Updated: 2022/01/05 10:26:54 by daniel        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ t_env *s_env, int nr_parts)
 			else if (i == nr_parts - 1)
 				*exit_status = ft_syntax_error_eof(s_env->line_nr);
 			else if (ft_strcmp(parts[i].part, "|") || !i \
-			|| ft_strchr(parts[i + 1].part, '|'))
+		|| (parts[i + 1].type == SPECIAL && ft_strchr(parts[i + 1].part, '|')))
 				*exit_status = ft_syntax_error(parts, i, s_env->line_nr);
 			else
 				*exit_status = ft_pipex(nr_parts, parts, s_env);
