94ce1df0ae1a0f19e7048c5aac9ad900ae96c668
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-01-06 10:43:54 +0100

    fix ft_calloc, check zeros before malloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:26:06 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2021/12/13 10:26:08 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/06 10:43:34 by daniel        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	ptr = malloc(count * size);
-	if (!ptr)
-		return (0);
 	if (count == 0 || size == 0)
 	{
 		count = 1;
 		size = 1;
 	}
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (0);
 	ft_bzero(ptr, count * size);
 	return (ptr);
 }
