02780680d9c6f48cc3e870ebe58f69686af4e6e0
StefdeRijk <sederi1998@gmail.com>
2022-01-06 13:59:08 +0100

    Tried to fix multiple redirections

--- redirect.c
+++ redirect.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 09:57:22 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/06 09:58:22 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/06 13:57:58 by sde-rijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ static int	ft_get_args(t_part *new_args, t_part *parts, char *c)
 	int	i;
 
 	i = 0;
-	while (ft_strcmp(parts->part, c) != 0)
+	c += 2;;
+	while (parts->part)
 	{
+		if ((!ft_strcmp(parts->part, "<") || !ft_strcmp(parts->part, "<<") \
+		|| !ft_strcmp(parts->part, ">") || !ft_strcmp(parts->part, ">>")) \
+		&& parts->type == SPECIAL)
+			parts += 2;
 		new_args->part = ft_strdup(parts->part);
 		parts++;
 		new_args++;
@@ -39,22 +44,16 @@ int	redirect_in(int nr_parts, t_part *parts, t_env *s_env, int *exec)
 	t_part	*new_args;
 	int		ret;
 	int		term;
-	int		fd;
-	int		i;
+	int		args;
 
-	i = 0;
 	*exec = 1;
-	while (ft_strcmp(parts[i].part, "<") != 0)
-		i++;
-	fd = open(parts[i + 1].part, O_RDONLY);
-	if (fd < 0)
-		return (ft_redir_error(SHELL_NAME, parts[i + 1].part, s_env->line_nr));
-	term = 1;
-	if (dup2(STDIN_FILENO, term) < 0 || dup2(fd, STDIN_FILENO) < 0)
-		return (ft_redir_error("dup2", "", s_env->line_nr));
 	new_args = ft_calloc((nr_parts - 1) * sizeof(*parts), 1);
-	i = ft_get_args(new_args, parts, "<");
-	ret = ft_executable(i, new_args, s_env);
+	args = ft_get_args(new_args, parts, "<");
+	term = 1;
+	if (dup2(STDIN_FILENO, term) < 0)
+		return (ft_redir_error("dup2", "", s_env->line_nr));
+	ret = ft_multiple_redir(parts, "<", 2, s_env->line_nr);
+	ret = ft_executable(args, new_args, s_env);
 	ft_free_parts(new_args);
 	if (dup2(term, STDIN_FILENO) < 0)
 		return (ft_redir_error("dup2", "", s_env->line_nr));
@@ -111,23 +110,48 @@ static int	ft_multiple_redir(t_part *parts, char *str, int append, int line_nr)
 	i = 1;
 	while (parts[i].part && ft_strcmp(parts[i].part, str) != 0)
 			i++;
-	if (append == 1)
+	if (append == 2)
+		fd = open(parts[i + 1].part, O_RDONLY);
+	else if (append == 1)
 		fd = open(parts[i + 1].part, O_RDWR | O_CREAT | O_APPEND, 0644);
 	else
 		fd = open(parts[i + 1].part, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (dup2(fd, STDOUT_FILENO) < 0)
-		return (ft_redir_error("dup2", "", line_nr));
+	if (fd < 0)
+			return (ft_redir_error(SHELL_NAME, parts[i + 1].part, line_nr));
+	if (!ft_strcmp(str, "<"))
+	{
+		if (dup2(fd, STDIN_FILENO) < 0)
+			return (ft_redir_error("dup2", "", line_nr));
+	}
+	else
+	{
+		if (dup2(fd, STDOUT_FILENO) < 0)
+			return (ft_redir_error("dup2", "", line_nr));
+	}
 	while (parts[i + 1].part && parts[i + 2].part && \
 	!ft_strcmp(parts[i + 2].part, str))
 	{
 		i++;
 		while (parts[i].part && ft_strcmp(parts[i].part, str) != 0)
 			i++;
-		fd = open(parts[i + 1].part, O_RDWR | O_CREAT | O_TRUNC, 0644);
+		if (!ft_strcmp(parts[i].part, "<"))
+			fd = open(parts[i + 1].part, O_RDONLY);
+		else if (!ft_strcmp(parts[i].part, ">>"))
+			fd = open(parts[i + 1].part, O_RDWR | O_CREAT | O_APPEND, 0644);
+		else
+			fd = open(parts[i + 1].part, O_RDWR | O_CREAT | O_TRUNC, 0644);
 		if (fd < 0)
 			return (ft_redir_error(SHELL_NAME, parts[i + 1].part, line_nr));
-		if (dup2(fd, STDOUT_FILENO) < 0)
-			return (ft_redir_error("dup2", "", line_nr));
+		if (!ft_strcmp(parts[i].part, "<"))
+		{
+			if (dup2(fd, STDIN_FILENO) < 0)
+				return (ft_redir_error("dup2", "", line_nr));
+		}
+		else
+		{
+			if (dup2(fd, STDOUT_FILENO) < 0)
+				return (ft_redir_error("dup2", "", line_nr));
+		}
 	}
 	return (0);
 }
