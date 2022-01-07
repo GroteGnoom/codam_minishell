e3df441c0451fce105adcff7bce6eeaeb3d26172
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-01-07 14:49:18 +0100

    norminette is_built_in

--- is_built_in.c
+++ is_built_in.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/15 12:54:54 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/07 14:12:21 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/07 14:48:48 by daniel        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 int	is_built_in(char *str, int nr_parts, t_part *parts, t_env *s_env)
 {
 	if (!ft_strcmp(str, "exit"))
-		return(ft_exit(nr_parts, parts, s_env->line_nr));
+		return (ft_exit(nr_parts, parts, s_env->line_nr));
 	else if (!ft_strcmp(str, "echo"))
-		return(ft_echo(nr_parts, parts));
+		return (ft_echo(nr_parts, parts));
 	else if (!ft_strcmp(str, "cd"))
-		return(ft_cd(parts, s_env->line_nr));
+		return (ft_cd(parts, s_env->line_nr));
 	else if (!ft_strcmp(str, "pwd"))
-		return(ft_pwd());
+		return (ft_pwd());
 	else if (!ft_strcmp(str, "env"))
-		return(ft_env(s_env->env));
+		return (ft_env(s_env->env));
 	else if (!ft_strcmp(str, "export"))
-		return(ft_export(parts, s_env));
+		return (ft_export(parts, s_env));
 	else if (!ft_strcmp(str, "unset"))
-		return(ft_unset(parts, s_env));
+		return (ft_unset(parts, s_env));
 	else
 		return (ft_executable(nr_parts, parts, s_env));
 }
