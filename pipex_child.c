627fb4dbc0d46ce367793ee780ff2d9a70375153
StefdeRijk <sederi1998@gmail.com>
2022-01-11 13:54:03 +0100

    Fixed segfault in pipex_child

--- pipex_child.c
+++ pipex_child.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:15:26 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/11 13:38:56 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/11 13:53:12 by sde-rijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,8 @@ int	ft_child_process(t_pipe pipex, int *pipefd, t_env *s_env, t_part *parts)
 	pipex.cmd_flag = ft_get_cmd_flag(parts, pipex, s_env, &status);
 	if (status)
 		exit(status);
-	status = is_built_in(pipex.cmd_flag[0].part, count_parts(pipex.cmd_flag), pipex.cmd_flag, s_env);
+	if (pipex.cmd_flag[0].part)
+		status = is_built_in(pipex.cmd_flag[0].part, count_parts(pipex.cmd_flag), pipex.cmd_flag, s_env);
 	free(pipex.cmd_flag);
 	return (status);
 }
