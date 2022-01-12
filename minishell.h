1b5112871c5ec8be3b8c2bca49dbb62f6d38a756
StefdeRijk <sederi1998@gmail.com>
2022-01-12 13:42:06 +0100

    Organized header file

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:16:05 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/12 13:30:35 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/12 13:41:56 by sde-rijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@ typedef struct s_part {
 
 typedef struct s_pipe
 {
+	int		len;
 	int		size;
 	int		iter;
-	int		len;
 	int		term_out;
 	int		term_in;
 
@@ -49,19 +49,23 @@ typedef struct s_env
 	int		line_nr;
 }	t_env;
 
-t_part	*ft_get_cmd_flag(t_part *parts, t_pipe pipex, \
-t_env *s_env, int *status);
+int		ft_pipex(int nr_parts, t_part *parts, t_env *s_env);
 int		ft_child_process(t_pipe pipex, int *pipefd, t_env *s_env, \
 t_part *parts);
-int		ft_pipex(int nr_parts, t_part *parts, t_env *s_env);
+t_part	*ft_get_cmd_flag(t_part *parts, t_pipe pipex, \
+t_env *s_env, int *status);
 t_part	*get_commands_between_pipes(t_part *parts, t_pipe pipex, \
 t_env *s_env);
+
 int		ft_is_redir(t_part part);
 int		is_pipe(t_part part);
 int		is_input_redir(t_part part);
 int		is_output_redir(t_part part);
 int		is_here_doc(t_part part);
+
 int		ft_multiple_redir(t_part *parts, int i, int line_nr, t_pipe pipex);
+int		ft_do_redir(t_part *parts, int line_nr, int i, t_pipe pipex);
+int		here_doc(char *final, int line_nr, t_part *parts, t_pipe pipex);
 
 void	signals(void);
 
@@ -71,45 +75,46 @@ int		ft_cd(t_part *parts, int line_nr);
 int		ft_env(char **envp);
 int		ft_export(t_part *parts, t_env *s_env);
 int		ft_exit(int nr_parts, t_part *parts, int line_nr);
-void	copy_env(char **envp, t_env *s_env);
 int		ft_unset(t_part *parts, t_env *s_env);
+void	copy_env(char **envp, t_env *s_env);
+
 char	*ft_wildcard(char *args, int *wild_quoted);
 char	**ft_split_unquoted(char const *s, char c, const int *quoted);
 
 int		ft_executable(int nr_parts, t_part *parts, t_env *s_env);
+void	ft_try_paths(char **paths, char **args, t_env *s_env, t_part *parts);
+char	**ft_get_paths(char **env);
+
+t_part	*quote_split(char *s);
+t_part	*ft_shell_split(char *s, int last_exit_status, t_env *s_env);
+
 void	expand_args(char **sp, int last_exit_status, t_env *s_env, \
 			int next_is_quoted);
-int		ft_export_print(char **envp);
-t_part	*quote_split(char *s);
 void	expand_unquoted_args(t_part *parts, int last_exit_status, t_env *s_env);
-char	**parts_to_strings(t_part *parts);
-t_part	*ft_shell_split(char *s, int last_exit_status, t_env *s_env);
-void	ft_free_parts(t_part *parts);
-int		count_parts(t_part *s);
-int		replace_parts(t_part **parts, int i);
+void	expand_wildcard(t_part **parts, int **wild_quoted);
 void	ft_replace(char **sp, int start, int len, char *rep);
-char	*ft_search_name(t_env *s_env, char *envname, int envlen);
+int		replace_parts(t_part **parts, int i);
+char	**parts_to_strings(t_part *parts);
+
+int		count_parts(t_part *s);
+void	ft_free_parts(t_part *parts);
 int		get_env_name_length(char *env);
 
-void	ft_try_paths(char **paths, char **args, t_env *s_env, t_part *parts);
-int		here_doc(char *final, int line_nr, t_part *parts, t_pipe pipex);
-char	**ft_get_paths(char **env);
-void	expand_wildcard(t_part **parts, int **wild_quoted);
-
+char	*ft_search_name(t_env *s_env, char *envname, int envlen);
 int		is_built_in(char *str, int nr_parts, t_part *parts, t_env *s_env);
 int		check_for_redirections(int *last_exit_status, t_part *parts, \
 t_env *s_env, int nr_parts);
+int		check_identifier(char *str, int unset);
 
 int		ft_syntax_error(t_part *parts, int i, int line_nr, char *token);
 int		ft_syntax_error_eof(int line_nr);
 int		ft_redir_error(char *str, char *str2, int line_nr);
-int		ft_do_redir(t_part *parts, int line_nr, int i, t_pipe pipex);
 int		ft_invalid_identifier(t_part *parts, int i, int line_nr);
-int		check_identifier(char *str, int unset);
 int		ft_exit_error(int too_many, char *arg, int line_nr);
 void	ft_print_line_nr(int line_nr);
-int		print_parts_error(t_part *parts);
 
 void	print_parts(t_part *parts);
+int		print_parts_error(t_part *parts);
 void	print_strs(char **strs);
+int		ft_export_print(char **envp);
 #endif
