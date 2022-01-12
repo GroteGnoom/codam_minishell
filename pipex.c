7312c24f9b087ca9fad3e0ac1e232e9abd8fdfb8
StefdeRijk <sederi1998@gmail.com>
2022-01-12 11:48:40 +0100

    Fixed some issues with multiple here_docs

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:15:43 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/12 11:11:39 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/12 11:24:04 by sde-rijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,21 @@ int	ft_pipex(int nr_parts, t_part *parts, t_env *s_env)
 {
 	t_pipe	pipex;
 	int		status;
-	int		term_out;
 	int		term_in;
 
 	pipex.begin = 0;
 	pipex.end = 0;
-	term_out = dup(STDOUT_FILENO);
+	pipex.term_out = dup(STDOUT_FILENO);
 	term_in = dup(STDIN_FILENO);
-	if (term_out < 0 || term_in < 0)
+	if (pipex.term_out < 0 || term_in < 0)
 		perror("dup");
 	pipex.len = nr_parts;
 	pipex.size = ft_get_size_parts(parts);
 	status = ft_pipex_pipe(pipex, s_env, parts);
-	if (dup2(term_out, STDOUT_FILENO) < 0 || dup2(term_in, STDIN_FILENO) < 0)
+	if (dup2(pipex.term_out, STDOUT_FILENO) < 0 || dup2(term_in, STDIN_FILENO) < 0)
 		perror("dup2");
 	close(term_in);
-	close(term_out);
+	close(pipex.term_out);
 	return (status);
 }
 
@@ -108,9 +107,9 @@ t_env *s_env, int status)
 			close(pipefd[0]);
 			close(pipefd[1]);
 		}
+		waitpid(child, &status, 0);
 		pipefd[0] = pipefd[2];
 		pipefd[1] = pipefd[3];
-		waitpid(child, &status, 0);
 		pipex.iter++;
 	}
 	if (pipex.size > 1)
