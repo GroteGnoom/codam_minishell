00078eda01be30b2cae61b85fb2219f12493f1fe
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-01-21 14:11:45 +0100

    fix newline error HOME not set

--- error_msg.c
+++ error_msg.c
@@ -6,7 +6,7 @@
 /*   By: daniel <marvin@codam.nl>                     +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/23 13:52:00 by daniel        #+#    #+#                 */
-/*   Updated: 2022/01/20 11:09:54 by dnoom         ########   odam.nl         */
+/*   Updated: 2022/01/21 14:11:32 by dnoom         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,7 @@ int	ft_home_not_set(t_part *parts, int i, int line_nr)
 	else
 		ft_print_line_nr(line_nr);
 	ft_putstr_fd(parts[i].part, 2);
-	ft_putstr_fd(": HOME not set", 2);
+	ft_putstr_fd(": HOME not set\n", 2);
 	return (1);
 }
 
