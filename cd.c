f359f42893f0d5cabf11f138598c1a3e26f98c92
Stef De rijk <sde-rijk@f1r4s2.codam.nl>
2022-01-24 13:05:32 +0100

    Added - option with cd

--- cd.c
+++ cd.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:17:01 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/24 12:54:11 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/24 13:04:16 by sde-rijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,12 @@ t_part *parts, int line_nr);
 
 static void	ft_set_pwds(char *old_dir, t_env *s_env);
 
+static int	ft_cd_home(t_env *s_env, t_part *parts);
+
+static int	ft_cd_oldpwd(t_env *s_env, t_part *parts);
+
 int	ft_cd(t_part *parts, int line_nr, t_env *s_env)
 {
-	char	*home_dir;
 	char	*old_dir;
 	int		err;
 	char	buf[PATH_MAX];
@@ -36,13 +39,9 @@ int	ft_cd(t_part *parts, int line_nr, t_env *s_env)
 	err = 0;
 	old_dir = getcwd(buf, PATH_MAX);
 	if (!parts[1].part)
-	{
-		home_dir = ft_search_name(s_env, "HOME", 4);
-		if (!home_dir)
-			err = ft_home_not_set(parts, 0, s_env->line_nr);
-		if (chdir(home_dir) < 0)
-			err = 1;
-	}
+		err = ft_cd_home(s_env, parts);
+	else if (parts[1].part == '-')
+		err = ft_cd_oldpwd(s_env, parts);
 	else if (parts[1].part[0] == '/')
 	{
 		if (chdir(parts[1].part))
@@ -53,6 +52,30 @@ int	ft_cd(t_part *parts, int line_nr, t_env *s_env)
 	if (err == 0)
 		ft_set_pwds(old_dir, s_env);
 	return (err);
+}
+
+static int	ft_cd_oldpwd(t_env *s_env, t_part *parts)
+{
+	char	*old_pwd;
+
+	old_pwd = ft_search_name(s_env, "OLDPWD", 7);
+	if (!old_pwd)
+		return (ft_home_not_set(parts, 0, s_env->line_nr, "OLDPWD"));
+	if (chdir(old_pwd) < 0)
+		return (1);
+	return (0);
+}
+
+static int	ft_cd_home(t_env *s_env, t_part *parts)
+{
+	char	*home_dir;
+
+	home_dir = ft_search_name(s_env, "HOME", 4);
+	if (!home_dir)
+		return (ft_home_not_set(parts, 0, s_env->line_nr, "HOME"));
+	if (chdir(home_dir) < 0)
+		return (1);
+	return (0);
 }
 
 static void	ft_set_pwds(char *old_dir, t_env *s_env)
