baf8619b1b4e1e64ab567c8d95863633af33511b
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-01-24 11:34:17 +0100

    one malloc less in pwd

--- pwd.c
+++ pwd.c
@@ -6,7 +6,7 @@
 /*   By: sde-rijk <sde-rijk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 10:15:03 by sde-rijk      #+#    #+#                 */
-/*   Updated: 2022/01/24 10:52:13 by sde-rijk      ########   odam.nl         */
+/*   Updated: 2022/01/24 11:32:45 by dnoom         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,31 +23,33 @@ int	print_old_pwd(t_env *s_env)
 	char	*pwd;
 	char	*print_str;
 	int		i;
+	int		err;
 
 	i = 0;
 	pwd = get_prev_var(s_env, "PWD", &i);
+	err = 0;
 	if (pwd)
+	{
 		print_str = ft_strjoin(pwd + 4, "\n");
-	if (!pwd || write(1, print_str, ft_strlen(print_str)) < 0)
-		return (1);
-	free(print_str);
-	return (0);
+		if (write(1, print_str, ft_strlen(print_str)) < 0)
+			err = 1;
+		free(print_str);
+		return (err);
+	}
+	return (1);
 }
 
 int	ft_pwd(t_env *s_env)
 {
-	char	*buf;
 	char	*print_str;
+	char	buf[PATH_MAX];
 
-	buf = NULL;
 	print_str = getcwd(buf, PATH_MAX);
-	free(buf);
 	if (!print_str)
 		return (print_old_pwd(s_env));
 	if (write(1, print_str, ft_strlen(print_str)) < 0)
 		return (1);
 	if (write(1, "\n", 1) < 0)
 		return (1);
-	free(print_str);
 	return (0);
 }
